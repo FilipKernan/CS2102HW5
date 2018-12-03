66210d3acdb5effbbb048e6514c4ccae9a2e7563
Filip Kernan <filip.kernan@gmail.com>
2018-12-02 21:56:11 -0500

    did some stuff

--- src/Reading.java
+++ src/Reading.java
@@ -1,2 +1,15 @@
 public class Reading {
+
+    Time time;
+    double degrees;
+    double deltaRainFall;
+
+    public Reading() {
+    }
+
+    public Reading(Time time, double degrees, double deltaRainFall){
+        this.time = time;
+        this.degrees = degrees;
+        this.deltaRainFall = deltaRainFall;
+    }
 }
--- src/WeatherMonitor.java
+++ src/WeatherMonitor.java
@@ -1,5 +1,12 @@
+import java.util.LinkedList;
+
 public class WeatherMonitor {
 
+    LinkedList<DailyWeatherReport> reports;
+
+    public WeatherMonitor() {
+        this.reports = new LinkedList<>();
+    }
 
     public double averageTempForMonth(){
         return 0.0;
@@ -7,5 +14,10 @@ public class WeatherMonitor {
 
     public double totalRailfallForMOnth(){
         return 0.0;
+    }
+
+    public WeatherMonitor addDailyReport(DailyWeatherReport report){
+        reports.add(report);
+        return this;
     }
 }
